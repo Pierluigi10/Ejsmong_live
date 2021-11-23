f9d15d51f7e3eb1abc2a2fe1ceba85373a925fc8
pierluigi <pierluigibaiano@gmail.com>
2021-11-23 11:58:40 +0100

    move folder views to src

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 app.use(express.static(staticDirectory));
 
 
-app.set("views", path.join(__dirname, "./public/views"));
+app.set("views", path.join(__dirname, "./src/views"));
 
 app.get("/", (req, res) => {
   res.render("index", {
