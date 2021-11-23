7dd156333a9a68727998792ecb0e588912ff75e8
pierluigi <pierluigibaiano@gmail.com>
2021-11-23 11:07:35 +0100

    install and import express and ejs. create index.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log("ok")
+import express from "express";
+import path from "path";
+
+const app = express();
+const __dirname = path.resolve(path.dirname(""));
+const port = 3044;
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './public/views'));
