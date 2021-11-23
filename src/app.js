11c45c9cea64793cb03d47ab1bdbab0afc45233a
pierluigi <pierluigibaiano@gmail.com>
2021-11-23 11:55:54 +0100

    add middleware to display static files

--- src/app.js
+++ src/app.js
@@ -4,8 +4,13 @@ import path from "path";
 const app = express();
 const __dirname = path.resolve(path.dirname(""));
 const port = 3044;
+const staticDirectory = path.join(__dirname, './public');
+app.use(express.static(staticDirectory));
 
 app.set("view engine", "ejs");
+app.use(express.static(staticDirectory));
+
+
 app.set("views", path.join(__dirname, "./public/views"));
 
 app.get("/", (req, res) => {
