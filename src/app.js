ef22b29c7a8481213a1a0cf4c3bcab07c19d1a53
pierluigi <pierluigibaiano@gmail.com>
2021-11-23 11:10:48 +0100

    listen to the port

--- src/app.js
+++ src/app.js
@@ -5,12 +5,15 @@ const app = express();
 const __dirname = path.resolve(path.dirname(""));
 const port = 3044;
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./public/views"));
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './public/views'));
+app.get("/", (req, res) => {
+  res.render("index", {
+    pageTitle: "Welcome",
+  });
+});
 
-app.get('/', (req, res) => {
-    res.render("index", {
-        pageTitle: "Welcome"
-    })
-})
+app.listen(port, () => {
+  console.log(`Now listening on port http://localhost:${port}`);
+});
